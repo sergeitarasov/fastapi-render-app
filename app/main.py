fea8bd625fc4fd7cdb3020b20410fba000207393
Sergei Tarasov <sergxf@yandex.ru>
2025-04-16 19:05:56 +0300

    2

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
+# from fastapi import FastAPI
+# from app.routers.string_utils import router as string_utils_router
+
+# app = FastAPI()
+
+# app.include_router(string_utils_router)
+
+# @app.get("/")
+# def read_root():
+#     return {"message": "Welcome to the FastAPI String Utils API!"}
+
+
 from fastapi import FastAPI
-from app.routers.string_utils import router as string_utils_router
+from pydantic import BaseModel
 
 app = FastAPI()
 
-app.include_router(string_utils_router)
+class TextInput(BaseModel):
+    text: str
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the FastAPI String Utils API!"}
+@app.post("/")
+async def count_characters(input: TextInput):
+    return {"character_count": len(input.text)}
